53ed3e36fa511c8643b4e2d2602aa978ae68f556
Ayaan Qayyum <96453808+qayyumayaan@users.noreply.github.com>
2025-02-24 14:12:00 -0500

    Add useful clipboard and saving features

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#app',
     data: {
         qrInput: '',
-        qrImage: null
+        qrImage: null,
+        copyText: "Copy to Clipboard",
+        downloadText: "Download QR Code"
     },
     methods: {
         generateQRCode: function() {
@@ -14,6 +16,75 @@ new Vue({
         reset: function() {
             this.qrInput = '';
             this.qrImage = null;
+            this.copyText = "Copy to Clipboard";
+            this.downloadText = "Download QR Code";
+        },
+        copyToClipboard: function() {
+            if (!this.qrImage) return;
+
+            let imgTag = this.$refs.qrContainer.querySelector('img');
+            if (!imgTag) return;
+
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+
+            let img = new Image();
+            img.crossOrigin = "Anonymous"; 
+            img.src = imgTag.src;
+
+            img.onload = () => {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+
+                canvas.toBlob((blob) => {
+                    if (!blob) return;
+
+                    const item = new ClipboardItem({ "image/png": blob });
+                    navigator.clipboard.write([item]).then(() => {
+                        this.copyText = "Copied!";
+                        setTimeout(() => {
+                            this.copyText = "Copy to Clipboard";
+                        }, 5000);
+                    }).catch(() => {
+                        this.copyText = "Failed!";
+                        setTimeout(() => {
+                            this.copyText = "Copy to Clipboard";
+                        }, 5000);
+                    });
+                }, "image/png");
+            };
+        },
+        downloadQRCode: function() {
+            if (!this.qrImage) return;
+
+            let imgTag = this.$refs.qrContainer.querySelector('img');
+            if (!imgTag) return;
+
+            let canvas = document.createElement('canvas');
+            let ctx = canvas.getContext('2d');
+
+            let img = new Image();
+            img.crossOrigin = "Anonymous"; 
+            img.src = imgTag.src;
+
+            img.onload = () => {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+
+                let link = document.createElement('a');
+                link.href = canvas.toDataURL("image/png");
+                link.download = 'QRCode.png';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+
+                this.downloadText = "Saved!";
+                setTimeout(() => {
+                    this.downloadText = "Download QR Code";
+                }, 5000);
+            };
         }
     }
 });
